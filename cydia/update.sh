146977b3699c737877ac9ba41f525ece7742e432
CODYQX4 <admin@codyqx4.com>
2016-02-19 15:38:17 -0500

    no message

--- cydia/update.sh
+++ cydia/update.sh
@@ -3,6 +3,7 @@
 # Clean Packages
 rm -rf deb-packed/*
 rm -rf Packages.bz2
+rm -rf Packages.gz
 
 # Build Packages
 cd deb-extracted
@@ -13,3 +14,4 @@ cp deb-mirror/*.deb deb-packed
 # Create Package Metadata
 dpkg-scanpackages deb-packed > Packages
 bzip2 -k Packages
+gzip -k Packages
